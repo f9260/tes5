d60cd8fe90ce66eabb9322d39efc926c2f449eef
f9260 <161130407+f9260@users.noreply.github.com>
2024-10-15 18:01:02 +0300

    Update menuData.tsx

--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -33,8 +33,8 @@ const menuData: Menu[] = [
   },
   {
     id: 6,
-    title: "Demo",
-    path: "https://forum.ve3.plus/d/3-demo-request",
+    title: "Buy on Fiverr",
+    path: "https://www.fiverr.com/s/Gz9lgr0",
     newTab: true,
   },
   {
